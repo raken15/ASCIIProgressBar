0077e345f7c14a3952ae8c73499904d984a901e1
Raviv <raken100@gmail.com>
2025-02-20 19:57:50 +0200

    Added the logic for the progress bar using input string and showing the substring as the progress until the string is filled and the progress bar is complete

--- ASCIIProgressBarApp/Program.cs
+++ ASCIIProgressBarApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string to display in the progress bar: ");
+        string inputString = Console.ReadLine() ?? ""; // Get user input
+        int total = inputString.Length; // Progress bar length matches string length
+
+        Console.WriteLine("\nLoading...");
+
+        for (int i = 0; i <= total; i++)
+        {
+            Console.Write("\r["); // Start of the progress bar
+            Console.Write(inputString.Substring(0, i)); // Revealed part of the string
+            Console.Write(new string('-', total - i)); // Unrevealed part as '-'
+            Console.Write($"] {i * 100 / total}%"); // Percentage display
+
+            Thread.Sleep(200); // Simulate loading delay
+        }
+
+        Console.WriteLine("\nDone!");
+    }
+}
